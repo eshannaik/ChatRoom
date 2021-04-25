05faeca6242a5ba3473cd77389f0fbc51ba6d3d2
Eshan <eshannaik96@gmail.com>
2021-04-25 19:38:07 +0530

    ChatRoom

---
+++ room/routing.py
@@ -0,0 +1,9 @@
+##connecting to consumer
+
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/room/(?P<room_name>\w+)/$',consumers.ChatRoomConsumer.as_asgi()),
+]
